36d22ad25d78a278baa510997c4d98126c9a8a70
akakou <akakou571@gmail.com>
2017-07-16 00:43:41 +0900

    [add] event

---
+++ js/event.js
@@ -0,0 +1,7 @@
+/* event.js
+this source program is called when nynko have event */
+
+/* when change selected radio button, called */
+function onChangeRadioButton(){
+  view.trunButton();
+}
--- js/view.js
+++ js/view.js
@@ -2,23 +2,25 @@
 this source program run for view*/
 
 
-/* check setup complete ,
-  and turn on button. */
-function trunButton(){
-  // get element
-  var server_radio = document.getElementById('server_radio');
-  var client_radio = document.getElementById('client_radio');
-  var udp_radio = document.getElementById('udp_radio');
-  var tcp_radio = document.getElementById('tcp_radio');
-
-  // if setup complete,
-  // meow button turn on(make it blue)
-  if((server_radio.checked || client_radio.checked) && (udp_radio.checked || tcp_radio.checked)){
+var view = {
+  /* check setup complete ,
+    and turn on button. */
+  trunButton: function(){
     // get element
-    var meow_button = document.getElementById('meow_button');
-    // remove btn-light
-    meow_button.classList = ['btn'];
-    // turn on canMeow of state
-    state.canMeow = true;
+    var server_radio = document.getElementById('server_radio');
+    var client_radio = document.getElementById('client_radio');
+    var udp_radio = document.getElementById('udp_radio');
+    var tcp_radio = document.getElementById('tcp_radio');
+
+    // if setup complete,
+    // meow button turn on(make it blue)
+    if((server_radio.checked || client_radio.checked) && (udp_radio.checked || tcp_radio.checked)){
+      // get element
+      var meow_button = document.getElementById('meow_button');
+      // remove btn-light
+      meow_button.classList = ['btn'];
+      // turn on canMeow of state
+      state.canMeow = true;
+    }
   }
 }
